fd0b590da8482e1e08f0f44763b77f30b4d8dc81
dannjb <deeja@users.noreply.github.com>
2025-07-06 16:16:04 +1200

    Added DHT11 code

--- examples/FireBeetle2C6/FireBeetle2C6.ino
+++ examples/FireBeetle2C6/FireBeetle2C6.ino
@@ -1,10 +1,16 @@
+/*
+Example of using a DHT11 to measure humidity and temperature on a FireBeetle2 ESP32 C6
+*/
 #include <BtHomeV2Device.h>
 #include <ArduinoBLE.h>
+#include <DHT11.h>
 #define SLEEP_DURATION_SECONDS 10
 #define BATTERY_MEASUREMENT_PIN 0
+#define DHT_POWER_PIN 19
+#define DHT_DATA_PIN 20
 
-const float V_CUTOFF = 3000;  // 0% SoC
-const float V_FULL = 4200;    // 100% SoC
+const float V_CUTOFF = 3000;  // 0% batteryPercentage
+const float V_FULL = 4200;    // 100% batteryPercentage
 
 /**  This code should work with most devices.
 FireBeetle 2 Notes
@@ -15,37 +21,28 @@ FireBeetle 2 Notes
  */
 RTC_DATA_ATTR uint64_t counter = 0;
 
+
 void setup() {
-  // initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
+  
+  // turn on sensor as it needs time to wake up
+  pinMode(DHT_POWER_PIN, OUTPUT);
+  digitalWrite(DHT_POWER_PIN, HIGH);
 
   // turn on LED
   pinMode(D13, OUTPUT);
   digitalWrite(D13, HIGH);
 
-  // set the resolution to 12 bits (0-4096)
-  //  read the analog / millivolts value for pin 0:
   analogReadResolution(12);
-  int analogValue = analogRead(BATTERY_MEASUREMENT_PIN);
   int analogMillivolts = analogReadMilliVolts(BATTERY_MEASUREMENT_PIN) * 2;
 
   delay(50);
-  // print out the values you read:
-  Serial.print("ADC analog value = ");
-  Serial.println(analogValue);
-  Serial.print("ADC millivolts value = ");
-  Serial.print(analogMillivolts);
-  Serial.println("mV");
-  // Please adjust the calculation coefficient according to the actual measurement.
   Serial.print("BAT millivolts value = ");
   Serial.print(analogMillivolts);
   Serial.println("mV");
-  Serial.println("--------------");
 
-  float soc = float(analogMillivolts - V_CUTOFF)
-              / float(V_FULL - V_CUTOFF)
-              * 100.0;
-  soc = constrain(soc, 0.0, 100.0);
+  float batteryPercentage = float(analogMillivolts - V_CUTOFF) / float(V_FULL - V_CUTOFF) * 100.0;
+  batteryPercentage = constrain(batteryPercentage, 0.0, 100.0);
 
   if (!BLE.begin()) {
     // If BLE doesn't start, then just go to sleep
@@ -54,21 +51,43 @@ void setup() {
     esp_deep_sleep_start();
   }
 
+  // set up the buffer for the advertisement data
   uint8_t advertisementData[MAX_ADVERTISEMENT_SIZE];
   uint8_t size = 0;
 
   // first advertisement with battery percentage
   BtHomeV2Device device("BEETLE", "My Firebeetle V2", false);
-  device.addBatteryPercentage(soc);
-  device.addWaterLitres(analogValue);
+  device.addBatteryPercentage(batteryPercentage);
+  device.addCount_0_4294967295(counter++);
   size = device.getAdvertisementData(advertisementData);
   sendAdvertisement(advertisementData, size);
+  device.clearMeasurementData(); // clear the buffer before the next advertisement
+
+  DHT11 dht11(DHT_DATA_PIN); // Initialise the DHT code
+
+  int temperature = 0;
+  int humidity = 0;
+
+  int result = dht11.readTemperatureHumidity(temperature, humidity);
+
+  // power off sensor
+  digitalWrite(DHT_POWER_PIN, LOW);
+
+  if (result == 0) {
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.print(" °C\tHumidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+  } else {
+    Serial.println(DHT11::getErrorString(result));
+  }
 
   // second advertisement with the voltage
   float volts = analogMillivolts / 1000.0f;
-  device.addVoltage_0_to_65_resolution_0_001(volts);
   device.addVoltage_0_to_6550_resolution_0_1(volts);
-  device.addCount_0_4294967295(counter++);
+  device.addTemperature_neg327_to_327_Resolution_0_01(temperature);
+  device.addHumidityPercent_Resolution_1(humidity);
 
   size = device.getAdvertisementData(advertisementData);
   sendAdvertisement(advertisementData, size);
